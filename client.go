df26844328eb9043d8bdb29bb27a3dd66be8d040
Christian Fotteler <chris@inilab.dk>
2025-06-26 16:15:54 +0200

    Add logging

--- client.go
+++ client.go
@@ -156,7 +156,11 @@ func Init(file string, ServerIndex string) {
 			newConfigPath := Config.Global.Software.AutoProvisioning.SaveFilePath + Config.Global.Software.AutoProvisioning.SaveFilename
 			log.Printf("info: Loading provisioned config from: %s", newConfigPath)
 			ConfigXMLFile = newConfigPath
-			readxmlconfig(ConfigXMLFile, false)
+			readxmlconfig(ConfigXMLFile, true)
+			log.Printf("debug: After autoprovisioning - Name[%d]: %s", AccountIndex, Name[AccountIndex])
+			log.Printf("debug: After autoprovisioning - Server[%d]: %s", AccountIndex, Server[AccountIndex])
+			log.Printf("debug: After autoprovisioning - Username[%d]: %s", AccountIndex, Username[AccountIndex])
+
 		}
 	}
 
